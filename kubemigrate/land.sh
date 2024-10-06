70a2d2fa2f22fd4355c3dd6f59a63337faf248a5
Student User <student@workstation.lab.example.com>
2024-10-06 00:08:45 -0400

    New update on 06-10-24

--- kubemigrate/land.sh
+++ kubemigrate/land.sh
@@ -1,4 +1,5 @@
 podman build --format docker -t redhat-landing demo > /dev/null
 podman save redhat-landing -o landing.tar > /dev/null
 podman rmi redhat-landing > /dev/null
+podman rmi registry.access.redhat.com/ubi8/ubi:8.0 > /dev/null
 echo "landing image tarball created at $(pwd)"
